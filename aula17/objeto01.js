4f71f08c69b3339957978c2d6a86f66c2380d1ff
ridoolfi <angeloggc2@gmail.com>
2023-11-12 19:11:41 -0300

    Aula e Ex String + Aula Numeros

--- aula17/objeto01.js
+++ aula17/objeto01.js
@@ -1,5 +1,47 @@
-let amigo = {nome: 'José',sexo: 'm', peso: 85.4, engordar(p){
-    this.peso += p
-}}
+/* let umaString = "um 'Texto'";
+console.log(umaString);*/
 
-console.log(typeof amigo)
+/*let umaString = "um Texto";
+console.log(umaString.charAt(7));*/
+
+/*let umaString = "um Texto";
+console.log(umaString.concat(' ', 'em', ' ', 'um'));*/
+
+/*let umaString = "um Texto";
+console.log(umaString.concat(' em um lindo dia'));
+console.log(umaString + ' em um lindo dia');
+console.log(`${umaString} em um lindo dia`);*/
+
+/*let umaString = "um texto";
+console.log(umaString.indexOf('o', 3));
+console.log(umaString.lastIndexOf('m', 3)); // Começa de trás para a frente*/
+
+/*let umaString = "um Texto";
+console.log(umaString.match(/[a-z]/));
+console.log(umaString.search(/x/));
+console.log(umaString.replace('um', 'Outra'));*/
+
+/*let umaString = "O rato roeu a roupa do rei de roma.";
+console.log(umaString.replace(/r/g, '#'));*/
+
+/*let umaString = 'O rato roe';
+console.log(umaString.length);*/
+
+/*let umaString = 'O rato roeu a roupa do rei de roma';
+console.log(umaString.slice(2,6));
+console.log(umaString.slice(-3));
+console.log(umaString.slice(-5));
+console.log(umaString.slice(-5, -1));
+console.log(umaString.substring(umaString.length -5, umaString.length -1));
+*/
+
+/*let umaString = 'O rato roeu a roupa do rei de roma.';
+console.log(umaString.split(' ', 3));*/
+
+let umaString = 'O rato roeu a roupa do rei de roma.';
+console.log(umaString.toUpperCase());
+console.log(umaString.toLowerCase());
+
+// Estudar pelo W3Schools ou Pelo MDN da mozilla
+
+
---
+++ aula18/index.js
@@ -0,0 +1,28 @@
+// IEEE 754-2008
+
+let num1 = 0.7; // number
+let num2 = 0.1; //number
+
+num1 = ((num1 * 100) + (num2 * 100)) / 100; // 0.8
+num1 = ((num1 * 100) + (num2 * 100)) / 100; // 0.8
+num1 = ((num1 * 100) + (num2 * 100)) / 100; // 0.8
+
+
+
+
+
+console.log(num1);
+console.log(Number.isInteger(num1));
+
+
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+// console.log(num1.toString(2));
+// console.log(num1.toFixed(4));
+// console.log(Number.isInteger(num1));
+
+
+// let temp = num1 * '5';
+// console.log(temp);
+
+
